4c03d508fa28a3e4cdb1ea7749fbe35758ec90a4
Ningyu He <pluton.he@gmail.com>
2021-11-19 19:19:53 +0800

    Add convert propagation logic bombs samples

---
+++ c_samples.nosync/source/df2cf_cp_l1.c
@@ -0,0 +1,67 @@
+/*
+TOY:
+*/
+#include <string.h>
+
+#define BOMB_ENDING 3
+#define NORMAL_ENDING 0
+
+int df2cf(char a)
+{
+  int b;
+  switch (a)
+  {
+  case 0:
+    b = 0;
+    break;
+  case 1:
+    b = 1;
+    break;
+  case 2:
+    b = 2;
+    break;
+  case 3:
+    b = 3;
+    break;
+  case 4:
+    b = 4;
+    break;
+  case 5:
+    b = 5;
+    break;
+  case 6:
+    b = 6;
+    break;
+  case 7:
+    b = 7;
+    break;
+  case 8:
+    b = 8;
+    break;
+  case 9:
+    b = 9;
+    break;
+  default:
+    b = 0;
+    break;
+  }
+  return b;
+}
+
+// {"s":{"length": 4}}
+int logic_bomb(char *s)
+{
+  int symvar = s[0] - 48;
+  int a = df2cf(symvar % 10);
+  a++;
+  int b = symvar + a;
+  if (b == 15)
+    return BOMB_ENDING;
+  else
+    return NORMAL_ENDING;
+}
+
+int main(int argc, char **argv)
+{
+  return logic_bomb(argv[1]);
+}
