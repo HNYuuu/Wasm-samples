d2bc09b21bc76dd37ac8c6a3044156d1d6c9c928
Ningyu He <pluton.he@gmail.com>
2021-11-27 17:51:45 +0800

    Updated buffer overflow samples

--- c_samples.nosync/source/heap_bo_l1.c
+++ c_samples.nosync/source/heap_bo_l1.c
@@ -1,5 +1,6 @@
 #include <string.h>
 #include <stdlib.h>
+#include <stdio.h>
 #define BOMB_ENDING 3
 #define NORMAL_ENDING 0
 
@@ -25,7 +26,14 @@ int logic_bomb(char *symvar)
     }
 }
 
-int main(int argc, char **argv)
+int main()
 {
-    return logic_bomb(argv[1]);
+    char argv[64];
+    int i;
+    // I choose 10 as the loop maximum limitation
+    for (i = 0; i < 20; i++)
+    {
+        scanf("%c", &argv[i]);
+    }
+    return logic_bomb(argv);
 }
--- c_samples.nosync/source/stack_bo_l2.c
+++ c_samples.nosync/source/stack_bo_l2.c
@@ -1,5 +1,6 @@
 #include <string.h>
 #include <stdlib.h>
+#include <stdio.h>
 #define BOMB_ENDING 3
 #define NORMAL_ENDING 0
 
@@ -18,7 +19,14 @@ int logic_bomb(char *symvar)
     return NORMAL_ENDING;
 }
 
-int main(int argc, char **argv)
+int main()
 {
-    return logic_bomb(argv[1]);
+    char argv[64];
+    int i;
+    // I choose 10 as the loop maximum limitation
+    for (i = 0; i < 10; i++)
+    {
+        scanf("%c", &argv[i]);
+    }
+    return logic_bomb(argv);
 }
